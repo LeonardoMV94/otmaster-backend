4ab3b7e5455d7c08f303c0dc778df57e3035898f
Leonardo Muñoz <contacto@pctester.cl>
2022-06-25 00:46:42 -0400

    dist

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import {logErrors, errorHandler,boomErrorHandler, ormErrorHandler} from './middl
 import {checkApiKey} from './middlewares/auth.handler.js';
 import cors from 'cors';
 
+import path from 'path';
+import {fileURLToPath} from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 //inicializar
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 //settings
 app.use(express.json());
-
 app.use(cors());
+app.use(express.static(__dirname + './../dist'));
 
 // auth strategies
 import './utils/auth/index.js'
@@ -19,6 +24,11 @@ import './utils/auth/index.js'
 // test
 app.get('/test', checkApiKey, (req, res) => {
     res.send('Hola, soy una nueva ruta de test')
+})
+
+// quasar dist
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'))
 })
 
 //routers
