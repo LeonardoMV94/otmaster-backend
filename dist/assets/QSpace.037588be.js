72bf1a34c1b4054ad1b84e23bd7f8eb130b9ec09
Leonardo Muñoz <leonardoeleazar.mv@gmail.com>
2022-07-18 16:54:36 -0400

    dist upgrade tableedit

--- dist/assets/QSpace.037588be.js
+++
@@ -1,1 +0,0 @@
-import{ae as a,bD as e}from"./index.ff13323e.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
---
+++ dist/assets/QSpace.0a5fb3f9.js
@@ -0,0 +1,1 @@
+import{ae as a,bD as e}from"./index.08d974d9.js";const s=a("div",{class:"q-space"});var c=e({name:"QSpace",setup(){return()=>s}});export{c as Q};
