a2018d02a2108ab9d3a7b85901ea0cdb9195df12
Leonardo Muñoz <leonardoeleazar.mv@gmail.com>
2022-07-21 12:05:07 -0400

    DispositivosPage

---
+++ dist/assets/Notifications.79b82ee7.js
@@ -0,0 +1,1 @@
+import{cd as o}from"./index.b87efa9c.js";const a=(e,t)=>{o.create({message:`${e}`,type:t,actions:[{label:"Cerrar",color:"white"}]})};export{a as c};
--- dist/assets/Notifications.cd9f2894.js
+++
@@ -1,1 +0,0 @@
-import{cc as o}from"./index.5404b0db.js";const a=(e,t)=>{o.create({message:`${e}`,type:t,actions:[{label:"Cerrar",color:"white"}]})};export{a as c};
