fdc8c737bce9ee2ec519144cc2f152b8608bc531
Leonardo Muñoz <leonardoeleazar.mv@gmail.com>
2022-07-21 20:29:49 -0400

    formTicket fixed

---
+++ dist/assets/Notifications.3fd28b16.js
@@ -0,0 +1,1 @@
+import{cd as o}from"./index.6afc27b2.js";const a=(e,t)=>{o.create({message:`${e}`,type:t,actions:[{label:"Cerrar",color:"white"}]})};export{a as c};
--- dist/assets/Notifications.fda502fa.js
+++
@@ -1,1 +0,0 @@
-import{cd as o}from"./index.efe594f0.js";const a=(e,t)=>{o.create({message:`${e}`,type:t,actions:[{label:"Cerrar",color:"white"}]})};export{a as c};
